c8b443681109c45e7fafca04db11e7f04b4864bc
ShunyaSuzuki <anyushu2017@gmail.com>
2020-04-01 16:35:22 +0900

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,16 @@ import '../sass/app.scss';
 
 // js
 import 'bootstrap';
+import 'slick-carousel';
+import 'lightbox2';
+
+$(function($) {
+  $('.information-slide').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '40px',
+  });
+});
